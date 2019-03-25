934596f0a9e17d468936930efd153da26c0efb01
zahramajd <zahra.majdabadi95@gmail.com>
2019-03-26 01:37:39 +0430

    layer forward function

--- Layer.py
+++ Layer.py
@@ -11,4 +11,25 @@ class Layer:
         self.x_in = x_in
         self.w = w
         self.activation = activation
+
+    def forward(self):
+        x_out = np.dot(self.x_in, self.w)
+        for i in np.nditer(x_out, op_flags=['readwrite']):
+            i = self.activation_function(i)
+        return x_out
         
+    def activation_function(self, input):
+        if(self.activation == 'ReLU'):
+            return self.ReLU(input)
+        return 0
+
+    def ReLU(self,x):
+        if(x >= 0):
+            return x
+        return 0
+
+    def derivative_ReLU(self, x):
+        if(x >=0 ):
+            return 1
+        return 0
+    
--- main.py
+++ main.py
@@ -5,3 +5,12 @@ from softmax import Softmax
 from loss import Loss
 
 
+#TODO:
+# layer node 
+# softmax node
+# loss node
+
+n = Layer(np.array([[1,2,3],[3,2,1]]),np.array([[3,4,5,6],[3,4,5,6],[3,4,5,6]]),'ReLU')
+o = n.forward()
+print(o)
+
