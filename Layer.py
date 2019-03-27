a38d5da9ee6f22edb0815fe04441e21c8dbbeb1d
zahramajd <zahra.majdabadi95@gmail.com>
2019-03-27 22:24:56 +0430

    activation functions & their derivatives

--- Layer.py
+++ Layer.py
@@ -1,12 +1,11 @@
+import numpy as np
 
 # Xin: N*Din
 # Xout: N*Dout
 # W: Din*Dout
 # Xout = f(Xin*W)
-import numpy as np
 
-#TODO:
-# backward
+
 class Layer:
 
     def __init__(self, x_in, w, activation):
@@ -14,8 +13,23 @@ class Layer:
         self.w = w
         self.activation = activation
 
+    def ReLU(self,z):
+        return np.maximum(0,z)
+
+    def derivative_ReLU(self, dA, z):
+        dZ = np.array(dA, copy = True)
+        dZ[z <= 0] = 0
+        return dZ
+
+    def sigmoid(self, z):
+        return 1/(1+np.exp(-z))
+
+    def derivative_sigmoid(self, dA, z):
+        sig = self.sigmoid(z)
+        return dA * sig * (1 - sig)
+
     def forward(self):
-        x_out = np.dot(self.x_in, self.w)
+        x_out = np.matmul(self.x_in, self.w)
         for i in np.nditer(x_out, op_flags=['readwrite']):
             i = self.activation_function(i)
         return x_out
@@ -25,17 +39,18 @@ class Layer:
             return self.ReLU(input)
         return 0
 
-    def ReLU(self,x):
-        if(x >= 0):
-            return x
+    def derivative_activation_function(self, input):
+        if(self.activation == 'ReLU'):
+            return self.derivative_ReLU(input)
         return 0
 
-    def derivative_ReLU(self, x):
-        if(x >=0 ):
-            return 1
-        return 0
-    
     def backward(self,upstream_gradient):
-        local_gradient = 1
-        gradient = upstream_gradient * local_gradient
+        # upstream_gradient: N*Dout 
+        # local_gradient: Din*Dout : Wt
+        der_act = np.matmul(self.x_in, self.w)
+        for i in np.nditer(der_act, op_flags=['readwrite']):
+            i = self.derivative_activation_function(i)
+
+        local_gradient = np.matmul(der_act,self.w)
+        gradient =np.matmul(upstream_gradient * local_gradient)
         return gradient
